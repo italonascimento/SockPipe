130ca326adad9472d5440d95895649b3db84cb8d
eduardonuzzi <eduardomnuzzi@gmail.com>
2017-09-24 13:13:51 -0300

    added spaces

--- demo/server.js
+++ demo/server.js
@@ -3,11 +3,13 @@ const SockPipe = require('../src/sockpipe')
 const fs = require('fs')
 const index = fs.readFileSync('index.html')
 
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'})
   res.end(index)
 })
 server.listen(8080)
+
 
 new SockPipe({
   httpServer: server,
