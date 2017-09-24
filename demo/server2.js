7c7d1556f0de8b35c0f208a5187fc8a2c833306c
Italo Nascimento <italo.abnascimento@gmail.com>
2017-09-24 17:22:03 -0300

    experimenting with route operator

---
+++ demo/server2.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const { SockPipe } = require('../dist/sockpipe')
+const fs = require('fs')
+const index = fs.readFileSync('index.html')
+const { Observable } = require('rxjs')
+
+Observable.prototype.route = function(route, handle) {
+  return handle(
+    this.filter(msg => msg.type === route)
+  )
+}
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end(index)
+})
+server.listen(8080)
+
+new SockPipe({
+  httpServer: server,
+  debug: true,
+  open: (message$) =>
+    [
+      message$.route('hello', helloRoute),
+      message$.route('hello2', hello2Route),
+      message$.mapTo('success')
+    ]
+
+})
+  .start()
+
+function helloRoute(msg$) {
+  return msg$.mapTo('hello response')
+}
+
+function hello2Route(msg$) {
+  return msg$.mapTo('hello2 response')
+}
