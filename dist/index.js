d4c1035bde5486877eee278d620e1f0a3d547a2a
Ítalo Nascimento <italo.abnascimento@gmail.com>
2017-10-12 23:32:18 -0300

    remove ignored files

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sockpipe_1 = require("./sockpipe");
-exports.sockpipe = sockpipe_1.default;
-var createRouter_1 = require("./createRouter");
-exports.createRouter = createRouter_1.default;
-//# sourceMappingURL=index.js.map
